118198e4d94fff473ceb72869e9f0d16493712c5
HenryMarken <hmarken@ualberta.ca>
2023-01-22 20:28:54 -0700

    contact responsive

--- pages/contact.js
+++ pages/contact.js
@@ -18,19 +18,19 @@ function Contact () {
             <div className="h-fit w-full relative">
                 <img src="/images/contact-page-cropped.jpg"/>
                 <div className="absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <h1 className="text-6xl tracking-widest font-medium">CONTACT</h1>
+                    <h1 className="md:text-6xl text-4xl md:tracking-widest tracking-wide mt-10 md:m-0">CONTACT</h1>
                 </div>
                 <Navbar fluid={true} className="w-full rounded-none absolute bg-opacity-0 top-0 mt-5">
                 <Navbar.Brand >
-                    <img  src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+                    <img  src="logo.png" className="mr-3 h-6 sm:h-9" alt="HM Logo"/>
                     <span className="self-center whitespace-nowrap text-xl text-white"> Henry Marken </span>
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse>
-                    <Link href="/" className="md:text-2xl text-white hover:text- font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-150"> Home </Link>
-                    <Link href="/gallery" className="md:text-2xl text-white hover:text- font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-150"> Gallery </Link>
-                    <Link href="/photoshoots" className="md:text-2xl text-white font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-150"> Photoshoots </Link>
-                    <Link href="/about" className="md:text-2xl text-white font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-150"> About </Link>
+                    <Link href="/" className="md:text-2xl text-white font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 md:hover:scale-110 duration-150"> Home </Link>
+                    <Link href="/gallery" className="md:text-2xl text-white font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 md:hover:scale-110 duration-150"> Gallery </Link>
+                    <Link href="/photoshoots" className="md:text-2xl text-white font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 md:hover:scale-110 duration-150"> Photoshoots </Link>
+                    <Link href="/about" className="md:text-2xl text-white font-extralight font-roboto transition ease-in-out delay-150  hover:-translate-y-1 md:hover:scale-110 duration-150"> About </Link>
                     <Link href="/contact" className="md:text-2xl text-white underline font-extralight font-roboto"> Contact </Link>
                 </Navbar.Collapse>
                 </Navbar>
@@ -44,19 +44,19 @@ function Contact () {
               <form onSubmit={handleSubmit} name="contact-form">
                 <div className="form-group mb-6">
                   <label for="full-name" className="float-left">Name</label>
-                  <input id="full-name" name="name" type="text" className="md:h-16 form-control block w-full px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid rounded transition ease-in-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none dark:bg-gray-900 dark:border-slate-700"  />
+                  <input id="full-name" name="name" type="text" className="md:h-16 form-control block w-full px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid rounded transition ease-in-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none dark:bg-gray-900 dark:border-slate-700 text-black"  />
                 </div>
                 <div className="form-group mb-6">
                   <label htmlFor="email" className="float-left">Email</label>
-                  <input id="email" type="email" name="email" className="md:h-16 form-control block w-full px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid rounded transition ease-in-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none dark:bg-gray-900 dark:border-slate-700" />
+                  <input id="email" type="email" name="email" className="md:h-16 form-control block w-full px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid rounded transition ease-in-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none dark:bg-gray-900 dark:border-slate-700 text-black" />
                   <ValidationError prefix="Email" field="email" errors={state.errors}/>
                 </div>
                 <div className="form-group mb-6">
                   <label  className="float-left">Message</label>
-                  <textarea id="message" name="message" className="form-control block w-full px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid rounded transition ease-in-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none dark:bg-gray-900 dark:border-slate-700 " rows="10"></textarea>
+                  <textarea id="message" name="message" className="form-control block w-full px-3 py-1.5 text-base font-normal bg-white bg-clip-padding border border-solid rounded transition ease-in-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none dark:bg-gray-900 dark:border-slate-700 text-black " rows="10"></textarea>
                   <ValidationError prefix="Message" field="message" errors={state.errors}/>
                 </div>
-                <button type="submit" onClick={handleClick} disabled={state.submitting} className="w-full px-6 py-2.5 border dark:border-slate-700 dark:text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                <button type="submit" onClick={handleClick} disabled={state.submitting} className="w-full px-6 py-2.5 border dark:border-slate-700 dark:text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-white hover:text-black hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-700 active:shadow-lg transition duration-150 ease-in-out">
                   Send
                 </button>
               </form>
