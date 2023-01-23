03ab6a85e4d8cbd7c7f9cfe3638ae80134f817ed
HenryMarken <hmarken@ualberta.ca>
2023-01-22 17:36:58 -0700

    gallery responsive

--- pages/gallery.js
+++ pages/gallery.js
@@ -5,14 +5,14 @@ function Gallery () {
     return (
         <main>
             <div className="h-1/2 w-full relative">
-                <img src="/images/water-reflection.jpg" className="object-fill h-full w-full"/>
+                <img src="/images/water-reflection-cropped.jpg" className="object-fill h-full w-full"/>
                 <div className="absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <h1 className="text-6xl tracking-widest font-medium">GALLERY</h1>
+                    <h1 className="md:text-6xl text-4xl tracking-widest mt-5">GALLERY</h1>
                 </div>
                 <Navbar fluid={true} className="w-full rounded-none absolute bg-opacity-0 top-0 mt-5">
                 <Navbar.Brand >
-                    <img  src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
-                    <span className="self-center whitespace-nowrap text-xl text-white"> Henry Marken </span>
+                    <img  src="logo.png" className="mr-3 h-6 sm:h-9" alt="HM Logo"/>
+                    <span className="self-center whitespace-nowrap md:text-xl text-whit font-roboto"> Henry Marken </span>
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse>
@@ -25,23 +25,22 @@ function Gallery () {
                 </Navbar>
             </div>
 
-            <div className="grid grid-flow-row auto-rows-max grid-cols-3 gap-6 my-10 mx-10">
-        
-                <img src="/images/walterdale-night.jpg" className="object-fill"/>
-                <img src="/images/stadium-night.jpg" className="object-fill my-auto"/>
-                <img src="/images/walterdale-side.jpg" className="object-fill"/>
-                <img src="/images/highlevel-bike.jpg" className="object-fill my-auto"/>
-                <img src="/images/university-fall.jpg" className="object-fill my-auto"/>
-                <img src="/images/vancouver-ariel.jpg" className="object-fill my-auto"/>
-                <img src="/images/sydney-book.png" className="object-fill"/>
-                <img src="/images/brett-mountain.jpg" className="object-fill"/>
-                <img src="/images/jaret-mountain.jpg" className="object-fill"/>
-                <img src="/images/edmonton-color.jpg" className="object-fill"/>
-                <img src="/images/vancouver-bridge.jpg" className="object-fill"/>
-                <img src="/images/fall-path-cropped.jpg" className="object-fill my-auto"/>
-                <img src="/images/julia-flower.jpg" className="object-fill my-auto"/>
-                <img src="/images/towers-cold.jpg" className="object-fill my-auto"/>
-                <img src="/images/whytecliff-girls.jpg" className="object-fill my-auto"/>
+            <div className="md:grid grid-flow-row auto-rows-max grid-cols-3 gap-6 my-10 mx-10">
+                <img src="/images/walterdale-night.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/stadium-night.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/walterdale-side.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/highlevel-bike.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/university-fall.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/vancouver-ariel.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/sydney-book.png" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/brett-mountain.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/jaret-mountain.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/edmonton-color.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/vancouver-bridge.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/fall-path-cropped.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/julia-flower.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/towers-cold.jpg" className="object-fill my-10 md:my-auto"/>
+                <img src="/images/whytecliff-girls.jpg" className="object-fil my-10l md:my-auto"/>
                
             </div>
         </main>
